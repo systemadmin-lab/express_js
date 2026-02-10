a2c72e5024e35e1e628668b7121287db070c19c9
systemadmin-lab <rejuananik@gmail.com>
2026-02-10 02:17:41 -0800

    moutpats

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express =require('express');
 const handle = require('./handle');
 const app = express();
+const admin = express()
 app.locals.title = 'My Express App';
-
+admin.get('/dashboard',(req,res)=>{
+    req.send('admin dashboard');
+})
 app.get('/',handle);
+app.use('/admin',admin);    
 
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
+  
 }); 
