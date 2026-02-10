b60d9ad945826a115f5b1a21b2a5d8a8d3b56164
systemadmin-lab <rejuananik@gmail.com>
2026-02-10 01:56:39 -0800

    app.local

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express =require('express');
 const app = express();
-app.use(express.json());
+app.locals.title = 'My Express App';
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
-});
-app.post('/',(req,res)=>{
-    const data = req.body;
-    res.json({message:'Data received',data});
-    console.log(req.body);
 });
 
 app.listen(3000,()=>{
